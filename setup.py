39f1e2e7462b58b87aefb80685ba80df46784628
Rui Martins <rsmartins@infoportugal.impresa.pt>
2016-01-06 17:07:44 +0000

    Preparing version 1.2.0

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
 from setuptools import setup, find_packages
 
-setup(name="django-smart-selects",
+setup(name="wagtail-smart-selects",
       version="1.2.0",
       description="Django application to handle chained model fields.",
-      author="Patrick Lauber",
+      author="InfoPortugal S.A.",
       packages=find_packages(),
       include_package_data=True,
       )
